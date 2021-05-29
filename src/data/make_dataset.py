4fa33ce12a20c98620c0a0276edbb5efcc5e6c24
Akis Loumpourdis <akis@akiss-air.home>
2021-05-29 11:21:46 +0100

    Started the dvc pipelines

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -3,6 +3,10 @@ import click
 import logging
 from pathlib import Path
 from dotenv import find_dotenv, load_dotenv
+import os
+import yaml
+import pandas as pd
+import argparse
 
 
 @click.command()
@@ -15,6 +19,27 @@ def main(input_filepath, output_filepath):
     logger = logging.getLogger(__name__)
     logger.info('making final data set from raw data')
 
+    load_and_save()
+
+
+
+def read_params(config_path):
+    with open(config_path) as yaml_file:
+        config = yaml.safe_load(yaml_file)
+    return config
+
+def get_data(config_path):
+    config = read_params(config_path)
+    # print(config)
+    data_path = config["data_source"]["gd_source"]
+    df = pd.read_csv(data_path, sep=",", encoding='utf-8')
+    return df
+def load_and_save(config_path):
+    config = read_params(config_path)
+    df = get_data(config_path)
+    new_cols = [col.replace(" ", "_") for col in df.columns]
+    raw_data_path = config["load_data"]["raw_dataset_csv"]
+    df.to_csv(raw_data_path, sep=",", index=False, header=new_cols)
 
 if __name__ == '__main__':
     log_fmt = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
